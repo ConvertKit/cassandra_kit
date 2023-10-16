72695d182461166a8ce0c716eab49e6e69a3b1a0
Jessica Sumner <jess.r.sumner@gmail.com>
2023-10-16 14:10:05 -0400

    adds patch for kwargs and delegate
    Co-authored-by: Jenny Allar <jennyallar@gmail.com>

--- lib/cassandra_kit/util.rb
+++ lib/cassandra_kit/util.rb
@@ -66,8 +66,9 @@ module CassandraKit
       include ::Forwardable
 
       def delegate(*args, &block)
-        return super if args.one?
-        Module.instance_method(:delegate).bind(self).call(*args, &block)
+        puts "Hi JESS"
+        kwargs = args.pop if args.last.is_a? Hash # pull off the hash that is the last argument
+        Module.instance_method(:delegate).bind(self).call(*args, **kwargs, &block)
       end
     end
   end
